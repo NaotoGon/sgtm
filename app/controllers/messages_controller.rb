8b6a4b09443f5bf7dcd9e6f5c58320f7ed12998b
NaotoGon <naotogon5710@gmail.com>
2020-09-03 19:20:42 +0900

    ビュー編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,12 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
-    @messages = @room.messages.includes(:user)
+    @messages = Message.order("created_at DESC")
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
+    @message = Message.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room.id)
     else
@@ -19,6 +19,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :voice).merge(user_id: current_user.id)
   end
 end
