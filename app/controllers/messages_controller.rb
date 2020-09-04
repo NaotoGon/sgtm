0887a1f89b4b308cea448d030b57df8f3e479802
NaotoGon <naotogon5710@gmail.com>
2020-09-04 18:20:47 +0900

    メッセージ投稿

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,13 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
-    @messages = Message.where(room_id: @room.id)
+    @messages = Message.all.order(created_at: "DESC")
   end
 
   def create
+    @messages = Message.all.order(created_at: "DESC")
     @room = Room.find(params[:room_id])
-    @message = Message.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room.id)
     else
@@ -19,6 +20,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :voice).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :voice, :sgtm).merge(user_id: current_user.id)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  mount_uploader :voice, VoicesUploader
 end
